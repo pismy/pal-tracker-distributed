f7e60781014d6d4b44994e4593fe9769ffa1f53c
albertoruscalla <alberto.ruscalla@accenture.com>
2018-10-29 04:19:59 -0600

    Configured Hystrix circuit breaker in allocations, timesheets & backlog

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,33 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+    private final String endpoint;
+    private final Map<Long, ProjectInfo> id2Info = new ConcurrentHashMap<>();
 
-    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+    public ProjectClient(RestOperations restOperations, String endpoint) {
         this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.endpoint = endpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo pi = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        if(pi != null) {
+            id2Info.put(projectId, pi);
+        }
+        return pi;
     }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return id2Info.get(projectId);
+    }
+
 }
